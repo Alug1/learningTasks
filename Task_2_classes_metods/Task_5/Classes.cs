8e597737ac186185cbedad9a2e9ce4be55e63d17
Alug1 <alugovkin98@mail.ru>
2025-02-07 14:36:45 +0300

    Добавлен Task 5 в новую папку

---
+++ Task_2_classes_metods/Task_5/Classes.cs
@@ -0,0 +1,82 @@
+﻿using Task_2_classes_metods;
+
+class BookReader : Reader
+{
+
+    public string Name { get; set; }
+    public BookReader(string Name)
+    {
+        this.Name = Name;
+    }
+    public void ReturnBook(string book, Librarian librarian)
+    {
+        Console.WriteLine($"Читатель {Name} возвращает книгу \"{book}\" библиотекарю {librarian.Name}");
+        librarian.GetBook(book, this);
+    }
+
+    public void TakeBook(string book, Librarian librarian)
+    {
+        librarian.IssueBook(book, this);
+        Console.WriteLine($"Читатель {Name} получает книгу \"{book}\" у библиотекаря {librarian.Name}");
+    }
+}
+class BookLibrarian : Librarian, Administrator
+{
+
+    public string Name { get; set; }
+
+    public BookLibrarian(string Name)
+    {
+        this.Name = Name;
+    }
+    public void FindBook(string book)
+    {
+        Console.WriteLine($"Библиотекарь {Name} ищет книгу \"{book}\"");
+    }
+
+    public void GetBook(string book, Reader reader)
+    {
+        Console.WriteLine($"Библиотекарь {Name} принимает книгу \"{book}\" от читателя {reader.Name}");
+    }
+
+    public void IssueBook(string book, Reader reader)
+    {
+        Console.WriteLine($"Библиотекарь {Name} выдает книгу \"{book}\" читателю {reader.Name}");
+    }
+}
+class BookSupplier : Supplier, Reader
+{
+
+    public string Name { get; set; }
+    public BookSupplier(string Name)
+    {
+        this.Name = Name;
+    }
+    public void DeliverBooks(string book, Administrator administrator)
+    {
+        Console.WriteLine($"Поставщик {Name} привозит книгу \"{book}\" администратору {administrator.Name}");
+
+    }
+    public void ReturnBook(string book, Librarian librarian)
+    {
+        Console.WriteLine($"Поставщик {Name} возвращает книгу \"{book}\" библиотекарю {librarian.Name}");
+        librarian.GetBook(book, this);
+    }
+    public void TakeBook(string book, Librarian librarian)
+    {
+        librarian.IssueBook(book, this);
+        Console.WriteLine($"Поставщик {Name} получает книгу \"{book}\" у библиотекаря {librarian.Name}");
+    }
+}
+class BookAdmin : Administrator
+{
+    public string Name { get; set; }
+    public BookAdmin(string Name)
+    {
+        this.Name = Name;
+    }
+    public void FindBook(string book)
+    {
+        Console.WriteLine($"администратор {Name} ищет книгу \"{book}\"");
+    }
+}
---
+++ Task_2_classes_metods/Task_5/Task_5.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+internal class Task_5
+{
+    private static void Main(string[] args)
+    {
+        BookReader reader = new BookReader("Иван");
+        BookLibrarian librarian = new BookLibrarian("Алексей");
+        BookSupplier bookSupplier = new BookSupplier("Лаврентий");
+        BookAdmin bookAdmin = new BookAdmin("Анастасия");
+        reader.TakeBook("Мастер и Маргарита", librarian);
+        reader.ReturnBook("Мастер и Маргарита", librarian);
+        bookSupplier.DeliverBooks("1000 и 1 рецепт пива", bookAdmin);
+        bookAdmin.FindBook("Сборник анекдотов за 2003 год");
+        bookSupplier.TakeBook("Краткое руководство по C#, Том 16", librarian);
+    }
+}
+
+public interface Reader
+{
+    string Name { get; set; }
+    void TakeBook(string book, Librarian librarian);
+    void ReturnBook(string book, Librarian librarian);
+}
+public interface Librarian
+{
+    string Name { get; set; }
+    void IssueBook(string book, Reader reader);
+    void GetBook(string book, Reader reader);
+}
+public interface Supplier
+{
+    string Name { get; set; }
+    void DeliverBooks(string book, Administrator administrator);
+}
+public interface Administrator
+{
+    string Name { get; set; }
+    void FindBook(string book);
+}
+
+
